aab940b74e024cc95d0991e6ce474e61eb4539b3
Quaant <egor.zhulin333@gmail.com>
2026-02-17 20:56:28 +0300

    fix bugs on wind

---
+++ build/googletest/googlemock/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/egorz/source/repos/mp2-lab5-maps/googletest/googlemock
+# Build directory: C:/Users/egorz/source/repos/mp2-lab5-maps/build/googletest/googlemock
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("../googletest")
---
+++ build/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/egorz/source/repos/mp2-lab5-maps/googletest/googletest
+# Build directory: C:/Users/egorz/source/repos/mp2-lab5-maps/build/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
