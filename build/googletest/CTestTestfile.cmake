ecfd31844522435a93a974832530718303157d5e
Zhulin Egor <142201265+Quaant@users.noreply.github.com>
2026-02-17 20:58:57 +0300

    Delete build directory

--- build/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/egorz/source/repos/mp2-lab5-maps/googletest
-# Build directory: C:/Users/egorz/source/repos/mp2-lab5-maps/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- build/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/egorz/source/repos/mp2-lab5-maps/googletest/googletest
-# Build directory: C:/Users/egorz/source/repos/mp2-lab5-maps/build/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
