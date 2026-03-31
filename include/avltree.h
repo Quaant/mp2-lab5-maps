11a06acefff3e6671e8ba24246accaed5d076e51
Quaant <egor.zhulin333@gmail.com>
2026-03-31 11:54:45 +0300

    fix all, insert working vrode bi(mb height nepravilno)

--- include/avltree.h
+++ include/avltree.h
@@ -18,30 +18,28 @@ private:
             : Tree<Tkey, Tval>::Node(k, v, l, r, p), height(h) {}
         //разве для Node не нужен оператор=? 
     };
-  
+
     avlNode *head;
-    void updateHeight(avlNode *root){ 
-        root->height = 1 + std::max(root->left.height, root->right.height);
-    }
-    int balance(avlNode *root) {
-        if (root == nullptr){
-            return 0; 
-        }
-        return root->right.height - root.left.height; 
-    };
+
+    inline avlNode* to_avl(typename Tree<Tkey, Tval>::Node* node) {
+    return static_cast<avlNode*>(node);
+}
+    
     avlNode* LL(avlNode *root){
         if (root == nullptr || root->left == nullptr){ 
             return root; 
         }
-        avlNode* parent = root->parent; 
-        avlNode* left_child = root->left; 
-        avlNode* left_child_right = left_child->right; 
+        avlNode* parent = to_avl(root->parent); 
+        avlNode* left_child = to_avl(root->left); 
+        avlNode* left_child_right = to_avl(left_child->right); 
 
+        
         left_child->right = root; 
         root->left = left_child_right;
         left_child_right->parent = root; 
         root->parent = left_child; 
-    
+
+        
         left_child->parent = parent; 
         if(parent != nullptr){
         if (parent->right == root){ 
@@ -59,15 +57,15 @@ private:
         if (root == nullptr || root->right == nullptr){ 
             return root; 
         } 
-        if (root->right->left == nullptr){ 
+        if (to_avl(root->right->left) == nullptr){ 
             return RR(root); 
         }
 
-        avlNode* parent = root->parent; 
-        avlNode* left_child = root->left; 
-        avlNode* left_child_right = left_child->right; 
-        avlNode* lcr_left = left_child_right->left; 
-        avlNode* lcr_right = left_child_right->right; 
+        avlNode* parent = to_avl(root->parent); 
+        avlNode* left_child = to_avl(root->left); 
+        avlNode* left_child_right = to_avl(left_child->right); 
+        avlNode* lcr_left = to_avl(left_child_right->left); 
+        avlNode* lcr_right = to_avl(left_child_right->right); 
 
         left_child_right->left = left_child; 
         left_child->parent = left_child_right; 
@@ -97,88 +95,202 @@ private:
     
     }
     avlNode* RR(avlNode *root){
-        if (root == nullptr || root->left == nullptr){ 
-            return root; 
-        }
-        avlNode* parent = root->parent; 
-        avlNode* right_child = root->right; 
-        avlNode* right_child_left = right_child->left; 
+        // if (root == nullptr || root->left == nullptr){ 
+        //     return root; 
+        // }
+        // avlNode* parent = root->parent; 
+        // avlNode* right_child = root->right; 
+        // avlNode* right_child_left = right_child->left; 
 
-        right_child->left = root; 
-        root->parent = right_child; 
-        root->right = right_child_left; 
-        right_child_left->parent = root; 
+        // right_child->left = root; 
+        // root->parent = right_child; 
+        // root->right = right_child_left; 
+        // right_child_left->parent = root; 
 
-        right_child->parent = parent; 
-        if(parent != nullptr){
-        if(parent->left == root){ 
-            parent->left = right_child; 
-        }
-        else{ 
-            parent->right = right_child; 
+        // right_child->parent = parent; 
+        // if(parent != nullptr){
+        // if(parent->left == root){ 
+        //     parent->left = right_child; 
+        // }
+        // else{ 
+        //     parent->right = right_child; 
         
-        }}
-        updateHeight(root); 
-        updateHeight(right_child); 
+        // }}
+        // updateHeight(root); 
+        // updateHeight(right_child); 
 
-        return right_child; 
+        // return right_child; 
     }
     avlNode* RL(avlNode *root){
-        if (root == nullptr || root->right == nullptr){ 
-            return root; 
-        } 
-        if (root->right->left == nullptr){ 
-            return RR(root); 
-        }
-        avlNode* right_child = root->right; 
-        avlNode* parent = root->parent; 
-        avlNode* right_child_left = right_child->left; 
-        avlNode* rcl_left = right_child_left->left; 
-        avlNode* rcl_right = right_child_left->right; 
+        // if (root == nullptr || root->right == nullptr){ 
+        //     return root; 
+        // } 
+        // if (root->right->left == nullptr){ 
+        //     return RR(root); 
+        // }
+        // avlNode* right_child = root->right; 
+        // avlNode* parent = root->parent; 
+        // avlNode* right_child_left = right_child->left; 
+        // avlNode* rcl_left = right_child_left->left; 
+        // avlNode* rcl_right = right_child_left->right; 
 
-        right_child_left->left = root; 
-        right_child_left->parent = parent; 
-        right_child_left->right = right_child; 
-        right_child->parent = right_child_left; 
-        root->parent = right_child_left; 
+        // right_child_left->left = root; 
+        // right_child_left->parent = parent; 
+        // right_child_left->right = right_child; 
+        // right_child->parent = right_child_left; 
+        // root->parent = right_child_left; 
         
-        root->right = rcl_left; 
-        rcl_left->parent = root; 
-        right_child->left = rcl_right; 
-        rcl_right->parent = right_child; 
+        // root->right = rcl_left; 
+        // rcl_left->parent = root; 
+        // right_child->left = rcl_right; 
+        // rcl_right->parent = right_child; 
 
-        if(parent != nullptr){
-        if(parent->right == root){ 
-            parent->right = right_child_left;
+        // if(parent != nullptr){
+        // if(parent->right == root){ 
+        //     parent->right = right_child_left;
 
+        // }
+        // else{ 
+        //     parent->left = right_child_left;  
+        // }}
+        // updateHeight(root); 
+        // updateHeight(right_child); 
+        // updateHeight(right_child_left); 
+
+        // return right_child_left; 
+    }
+    void updateHeight(avlNode *root){ 
+        int a = 0, b = 0; 
+        if (root->left != nullptr){ 
+            a = to_avl(root->left)->height; 
+        }
+        if (root->right != nullptr){
+            b = to_avl(root->right)->height; 
+        }
+        root->height = 1 + std::max(a, b);
+    }
+    int balance(avlNode *root) {
+        if (root == nullptr){    
+            return 0; 
+        }
+
+        
+        //УМИРАЕМ ЗДЕСЬ!!!!
+        if (root->right != nullptr && root->left != nullptr){ 
+        return to_avl(root->right)->height - to_avl(root->left)->height;} 
+        else if (root->right == nullptr && root->left != nullptr){ 
+            return -to_avl(root->left)->height; 
+        } 
+        else if (root->left == nullptr && root->right != nullptr){ 
+            return to_avl(root->right)->height; 
         }
         else{ 
-            parent->left = right_child_left;  
-        }}
-        updateHeight(root); 
-        updateHeight(right_child); 
-        updateHeight(right_child_left); 
+            return 0; 
+        }
+    };
+    void rebalance(avlNode *root){ 
+        if (root == nullptr) {
+            return;}
+        avlNode* bal = root; 
 
-        return right_child_left; 
-    }
-    void Rotate(avlNode *root){ //эту просто будем запускать после вставки рекурсивно, для балансировки(в ней будут лежать LL LR RL RR ) 
-    }
+        while (bal != nullptr){ 
+
+            int balik = balance(bal);
+            std::cout << bal->data.key << ':' << balik << "\n";
+
+        if (balik == -2){ 
+            if (balance(static_cast<avlNode*>(bal->left)) == 1){ 
+                //LR 
+                bal = LR(bal); 
+                bal = static_cast<avlNode*>(bal->parent); 
+            }
+            else if (balance(static_cast<avlNode*>(bal->left)) == -1){ 
+                //LL
+                bal = LL(bal);
+                bal = static_cast<avlNode*>(bal->parent); 
+            }
+        }
+        else if (balik == 2){ 
+            if(balance(static_cast<avlNode*>(bal->right)) == 1){ 
+                // RR
+                bal = RR(bal); 
+                bal = static_cast<avlNode*>(bal->parent); 
+            } 
+            else if (balance(static_cast<avlNode*>(bal->right)) == -1){ 
+                // RL
+                bal = RL(bal);
+                bal = static_cast<avlNode*>(bal->parent); 
+            }
+        }
+        else{
+            updateHeight(bal); 
+            bal = static_cast<avlNode*>(bal->parent); 
+        }
+    }}
 public:
     avltree() : Tree<Tkey, Tval>(), head(nullptr) {}; 
     avltree(avlNode *h){
         head = h; 
         head->parent = nullptr; 
     } 
+
+    void _insert(const Tkey&k, const Tval&v){ 
+         if (head == nullptr){ 
+            head = new avlNode(k, v, nullptr, nullptr, nullptr, 1); 
+            return; 
+        }
+
+        avlNode* tmp = head; 
+        avlNode* prev = nullptr; 
+
+        while(tmp != nullptr){ 
+            prev = tmp; 
+            if (k < tmp->data.key)
+        {
+            
+            tmp = static_cast<avlNode*>(tmp->left);
+        }
+        else if (k > tmp->data.key)
+        {
+            tmp = static_cast<avlNode*>(tmp->right);
+        }
+        else
+        {
+            throw std::runtime_error("key is not unique");
+        }
+        }
+        avlNode *newNode = new avlNode(k, v, nullptr, nullptr, prev, 1);
+
+    if (k < prev->data.key)
+    {
+        prev->left = newNode;
+
+    }
+    else
+    {
+        prev->right = newNode;
+    }
+
+    rebalance(newNode); 
+    }
+
+    void insert(const Tkey &k, const Tval &v) override 
+    { 
+        _insert(k, v);
+    }
     void remove(size_t pos) override {};
     void remove(const Tkey &k) override {};
-    void insert(const Tkey &k, const Tval &v) override {
-    };
+
+
+    //~~~SEARCH~~~ 
     bool search(const Tkey &k) override
     {
         return true; // chtob ne otkisalo pri complire
     };
     Tval search_elem(const Tkey &k) override {};
 
+
+    //~~~~PRINT~~~~
     void printAvlTree()
     {
         this->printTree(this->head, "HEAD: ");
@@ -187,6 +299,9 @@ public:
     {
         this->printTreeWithKey(this->head);
     }
+
+
+    //~~OTHER~~~
     avlNode *findNodebyPosition(size_t pos)
     {
         //upcast - mozhno ne castit' 
@@ -202,6 +317,8 @@ public:
     {
         return static_cast<avlNode*>(this->findNodeByKey(k));
     }
+
+
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 #include "avltree.h" 
+#include <typeinfo> 
+
 
 using namespace std; 
 
 
 int main()
 {
+     
     avltree <int, double> t; 
-    t.printAvlTree();
+    t.insert(2, 2.0); 
+    cout << "!!!!!!!!!!!!!" << endl;
+    t.insert(1, 1.0); 
+    cout << "!!!!!!!!!!!!!" << endl;
+     t.insert(3, 3.0); 
+     cout << "!!!!!!!!!!!!!" << endl;
+     t.insert(4, 4.0); 
+     cout << "!!!!!!!!!!!!!" << endl;
+    t.printAvlTree(); 
     cout << "ok" << endl; 
     return 0;
 }
